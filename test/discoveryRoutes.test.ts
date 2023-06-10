cd7619b5d06ead3171d1092df925df64be81f822
Slawomir Piela <slawomir.piela@outlook.com>
2023-06-10 16:50:28 +0100

    tests to support closestproximity logic

--- test/discoveryRoutes.test.ts
+++ test/discoveryRoutes.test.ts
@@ -1,29 +1,62 @@
-import { sortAndLimitBusinessesByProximity, sortAndLimitBusinessesByProximityFilteredByCategory, sortBusinessesByProximity } from '../srv/routes/discoveryRoutes';
+import {
+  sortAndLimitBusinessesByProximity,
+  sortAndLimitBusinessesByProximityFilteredByCategory,
+  sortBusinessesByProximity,
+} from "../srv/routes/discoveryRoutes";
+//import businessesData from '../srv/data/businesses.json'
+import { Request, Response } from "express";
 
-//test the fxns logic to 
-describe('Proximity Routes - Unit Tests', () => {
+import {
+  createRequest,
+  createResponse,
+  MockRequest,
+  MockResponse,
+} from "node-mocks-http";
+
+//test the fxns logic to
+describe("Proximity Routes - Unit Tests", () => {
+  let request: MockRequest<Request>;
+  let response: MockResponse<Response>;
 
   beforeEach(() => {
-
+    /** Response Mock */
+    response = createResponse();
   });
 
-  describe('test sortBusinessesByProximity function', () => {
-    it('should create a new user', () => {
-        expect(1).toEqual(1);
+  describe("test sortBusinessesByProximity function", () => {
+    it("should sort businesses by proximity with success", () => {
+      const mockRequest = {
+        body: {
+          userLat: 15,
+          userLong: 15
+        },
+      } as Request;
+
+      request = createRequest({
+        method: "GET",
+        url: "/mockedRoute",
+      });
+
+      const resp = sortBusinessesByProximity(request, response);
+
+      expect(response._getStatusCode()).toEqual(200);
+      expect(response._getData()).toBeDefined();
+      expect(JSON.parse(response._getData())).toHaveProperty('success');
+      expect(JSON.parse(response._getData()).success).toBe(true);
+      expect(JSON.parse(response._getData())).toHaveProperty('location');
+      expect(JSON.parse(response._getData())).toHaveProperty('businesses');
     });
   });
 
-  describe('test sortAndLimitBusinessesByProximity function', () => {
-    it('should retrieve all users', () => {
+  describe("test sortAndLimitBusinessesByProximity function", () => {
+    it("should retrieve all users", () => {
       expect(1).toEqual(1);
     });
   });
 
-  describe('sortAndLimitBusinessesByProximityFilteredByCategory', () => {
-    it('should retrieve a user by ID', () => {
-
-        expect(1).toEqual(1);
+  describe("sortAndLimitBusinessesByProximityFilteredByCategory", () => {
+    it("should retrieve a user by ID", () => {
+      expect(1).toEqual(1);
     });
   });
-
 });
--- test/server.test.ts
+++ test/server.test.ts
@@ -1,34 +1,84 @@
-import request from 'supertest';
-import { app } from '../srv/server';
-import { sortAndLimitBusinessesByProximity, sortAndLimitBusinessesByProximityFilteredByCategory, sortBusinessesByProximity } from '../srv/routes/discoveryRoutes';
+import request from "supertest";
+import { startServer, stopServer, app } from "../srv/server";
+import {
+  sortAndLimitBusinessesByProximity,
+  sortAndLimitBusinessesByProximityFilteredByCategory,
+  sortBusinessesByProximity,
+} from "../srv/routes/discoveryRoutes";
+//jest.mock('../srv/routes/discoveryRoutes')
 
-  //mock here the return values off the fxns
-describe('Server Routes', () => {
+jest.setTimeout(20000);
+//mock here the return values off the fxns
+describe("Server Routes", () => {
+  let server: any;
+
+  beforeAll((done) => {
+    startServer(() => {
+      server = app;
+      done();
+    });
+  });
+
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
-  afterAll(() => {
-    jest.restoreAllMocks();
+  afterAll((done) => {
+    stopServer(done);
+    //jest.restoreAllMocks();
   });
 
+  describe("Test route - GET /discovery?lat=X&long=Y", () => {
+    it("should retrieve list of businesses sorted by proximity to my location - success", async () => {
+      // Mock the createUser function to return the created user
+      // (sortBusinessesByProximity as jest.Mock).mockImplementation(() => {
 
-  describe('Test route - GET /discovery?lat=X&long=Y', () => {
-    it('should retrieve list of businesses sorted by proximity to my location - success', async () => {
+      //   return ({
+      //     success : true,
+      //     outputBusinesses : [{
+      //       name: 'businessName1',
+      //       latitude: 'latCords',
+      //       longitude: 'longCords',
+      //       distance: 'distanceFromInput'
+      //     },
+      //     {
+      //       name: 'businessName2',
+      //       latitude: 'latCords',
+      //       longitude: 'longCords',
+      //       distance: 'distanceFromInput'
+      //     },
+      //     {
+      //       name: 'businessName3',
+      //       latitude: 'latCords',
+      //       longitude: 'longCords',
+      //       distance: 'distanceFromInput'
+      //     }]
+      //   });
+      // });
+
+      // const response = await request(server).get('/users');
+      // expect(response.status).toBe(200);
+      // expect(response.body).toEqual([]);
+
+      //const sortedBusinesses = sortBusinessesByProximity()
+      // const response = await request(app).get('/discovery')
+
+      // expect(response).toBeDefined();
+      // expect(response).toHaveProperty('success');
+      // expect(response).toHaveProperty('outputBusinesses');
       expect(1).toEqual(1);
     });
   });
 
-  describe('Test route - GET /discovery?lat=X&long=Y&limit=Z', () => {
-    it('should retrieve list of businesses sorted by proximity to my location, with limited number of records - success', async () => {
+  describe("Test route - GET /discovery?lat=X&long=Y&limit=Z", () => {
+    it("should retrieve list of businesses sorted by proximity to my location, with limited number of records - success", async () => {
       expect(1).toEqual(1);
     });
   });
 
-  describe('Test route - GET /discovery?lat=X&long=Y&limit=Z&type=Coffee', () => {
-    it('should retrieve list of businesses sorted by proximity to my location, with limited number of records & fitlered by type - success', async () => {
+  describe("Test route - GET /discovery?lat=X&long=Y&limit=Z&type=Coffee", () => {
+    it("should retrieve list of businesses sorted by proximity to my location, with limited number of records & fitlered by type - success", async () => {
       expect(1).toEqual(1);
     });
   });
-
 });
