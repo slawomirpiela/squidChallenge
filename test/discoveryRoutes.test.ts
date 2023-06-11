991793a5fef785abed553c19af0c0de97479a1f7
Slawomir Piela <slawomir.piela@outlook.com>
2023-06-11 22:28:20 +0100

    chore: improved tests

--- test/discoveryRoutes.test.ts
+++ test/discoveryRoutes.test.ts
@@ -1,11 +1,8 @@
 import {
-  sortAndLimitBusinessesByProximity,
-  sortAndLimitBusinessesByProximityFilteredByCategory,
   sortBusinessesByProximity,
 } from "../srv/routes/discoveryRoutes";
-//import businessesData from '../srv/data/businesses.json'
-import { Request, Response } from "express";
 
+import { Request, Response } from "express";
 import { createRequest, createResponse, MockRequest, MockResponse } from "node-mocks-http";
 
 //test the fxns logic to
@@ -20,51 +17,165 @@ describe("Proximity Routes - Unit Tests", () => {
 
   describe("test sortBusinessesByProximity function", () => {
     it("should sort businesses by proximity with success", () => {
-      const mockRequest = {
-        body: {
-          userLat: 15,
-          userLong: 15,
-        },
-      } as Request;
 
       request = createRequest({
         method: "GET",
         url: "/discovery?lat=53.341234&long=-6.258765",
       });
 
-      const resp = sortBusinessesByProximity(request, response);
+      sortBusinessesByProximity(request, response);
 
       expect(response._getStatusCode()).toEqual(200);
       expect(response._getData()).toBeDefined();
-      expect(JSON.parse(response._getData())).toHaveProperty("success");
-      expect(JSON.parse(response._getData()).success).toBe(true);
-      expect(JSON.parse(response._getData())).toHaveProperty("location");
-      expect(JSON.parse(response._getData())).toHaveProperty("businesses");
+      const retrievedData = JSON.parse(response._getData())
+      expect(retrievedData).toHaveProperty("success");
+      expect(retrievedData.success).toBe(true);
+      expect(retrievedData).toHaveProperty("closestBusinesses");
     });
 
-    it("should return a 400 Bad Request error while trying to sort businesses by proximity", () => {
+    it("should return a 400 Bad Request because coordinates are invalid - latitute is not a number!", () => {
+
       request = createRequest({
         method: "GET",
-        url: "/mockedRoute",
+        url: "/discovery?lat=human&long=-999.258765",
       });
 
       sortBusinessesByProximity(request, response);
 
       expect(response._getStatusCode()).toEqual(400);
       expect(response._getData()).toBeDefined();
-      expect(response._getData()).toBe("Invalid query parameters");
+      expect(response._getData()).toBe('Invalid latitude or longitude');
+    });
+
+    it("should return a 400 Bad Request because coordinates are invalid - longitude is not a number!", () => {
+
+      request = createRequest({
+        method: "GET",
+        url: "/discovery?lat=199&long=erroredLongVal",
+      });
+
+      sortBusinessesByProximity(request, response);
+
+      expect(response._getStatusCode()).toEqual(400);
+      expect(response._getData()).toBeDefined();
+      expect(response._getData()).toBe('Invalid latitude or longitude');
+    });
+
+    it("should return a 400 Bad Request because coordinates are invalid - latitute is a number but not a valid coordinates!", () => {
+
+      request = createRequest({
+        method: "GET",
+        url: "/discovery?lat=-199&long=-999.258765",
+      });
+
+      sortBusinessesByProximity(request, response);
+
+      expect(response._getStatusCode()).toEqual(400);
+      expect(response._getData()).toBeDefined();
+      expect(response._getData()).toBe('Invalid latitude or longitude');
+    });
+
+    it("should return a 400 Bad Request because coordinates are invalid", () => {
+
+      request = createRequest({
+        method: "GET",
+        url: "/discovery?lat=0.341234&long=-999.258765",
+      });
+
+      sortBusinessesByProximity(request, response);
+
+      expect(response._getStatusCode()).toEqual(400);
+      expect(response._getData()).toBeDefined();
+      expect(response._getData()).toBe('Invalid latitude or longitude');
+    });
+
+
+    it("should return a 400 Bad Request error while trying to sort businesses by proximity", () => {
+      request = createRequest({
+        method: "GET",
+        url: "/discovery",
+      });
+
+      sortBusinessesByProximity(request, response);
+
+      expect(response._getStatusCode()).toEqual(400);
+      expect(response._getData()).toBeDefined();
+      expect(response._getData()).toBe("Invalid basic query parameters - Please check you're providing the right coordinates");
     });
   });
 
-  describe("test sortAndLimitBusinessesByProximity function", () => {
-    it("should retrieve all users", () => {
-      expect(1).toEqual(1);
+  describe("test sortAndLimitBusinessesByProximity function with limiting parameter", () => {
+    it("should sort businesses by proximity & limit results with success", () => {
+
+      request = createRequest({
+        method: "GET",
+        url: "/discovery?lat=53.341234&long=-6.258765&limit=5",
+      });
+
+      sortBusinessesByProximity(request, response);
+
+      expect(response._getStatusCode()).toEqual(200);
+      expect(response._getData()).toBeDefined();
+      const retrievedData = JSON.parse(response._getData())
+      expect(retrievedData).toHaveProperty("success");
+      expect(retrievedData.success).toBe(true);
+      expect(retrievedData).toHaveProperty("closestBusinesses");
     });
+
+    it("should sort businesses by proximity & but ignore the limit number as its invalid", () => {
+
+      request = createRequest({
+        method: "GET",
+        url: "/discovery?lat=53.341234&long=-6.258765&limit=five",
+      });
+
+      sortBusinessesByProximity(request, response);
+
+      expect(response._getStatusCode()).toEqual(200);
+      expect(response._getData()).toBeDefined();
+      const retrievedData = JSON.parse(response._getData())
+      expect(retrievedData).toHaveProperty("success");
+      expect(retrievedData.success).toBe(true);
+      expect(retrievedData).toHaveProperty("closestBusinesses");
+    });
+
   });
 
-  describe("sortAndLimitBusinessesByProximityFilteredByCategory", () => {
-    it("should retrieve a user by ID", () => {
-      expect(1).toEqual(1);
+  describe("test sortAndLimitBusinessesByProximity function with filter parameter", () => {
+    it("should sort businesses by proximity & limit & filter results with success", () => {
+
+      request = createRequest({
+        method: "GET",
+        url: "/discovery?lat=53.341234&long=-6.258765&type=Coffee&limit=15",
+      });
+
+      sortBusinessesByProximity(request, response);
+
+      expect(response._getStatusCode()).toEqual(200);
+      expect(response._getData()).toBeDefined();
+      const retrievedData = JSON.parse(response._getData())
+      expect(retrievedData).toHaveProperty("success");
+      expect(retrievedData.success).toBe(true);
+      expect(retrievedData).toHaveProperty("closestBusinesses");
     });
+
+    it("should sort businesses by proximity & limit but ignore filter as its invalid", () => {
+
+      request = createRequest({
+        method: "GET",
+        url: "/discovery?lat=53.341234&long=-6.258765&type=Kava&limit=15",
+      });
+
+      sortBusinessesByProximity(request, response);
+
+      expect(response._getStatusCode()).toEqual(200);
+      expect(response._getData()).toBeDefined();
+      const retrievedData = JSON.parse(response._getData())
+      expect(retrievedData).toHaveProperty("success");
+      expect(retrievedData.success).toBe(true);
+      expect(retrievedData).toHaveProperty("closestBusinesses");
+    });
+
   });
+
 });
