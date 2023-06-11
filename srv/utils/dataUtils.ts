7163cb8d45c4c27c47e3a4c2eb7169e52846b04b
Slawomir Piela <slawomir.piela@outlook.com>
2023-06-11 21:40:12 +0100

    run prettify

--- srv/utils/dataUtils.ts
+++ srv/utils/dataUtils.ts
@@ -5,17 +5,17 @@ import fs from "fs";
 const businessesDataPath = "srv/data/businesses.json";
 
 export const retrieveBusinesses = (): Business[] => {
-    const fileContents = fs.readFileSync(businessesDataPath, "utf-8");
-    const businessData: BusinessData = JSON.parse(fileContents);
+  const fileContents = fs.readFileSync(businessesDataPath, "utf-8");
+  const businessData: BusinessData = JSON.parse(fileContents);
 
-    const businesses: Business[] = [];
+  const businesses: Business[] = [];
 
-    for (const key in businessData) {
-        if (businessData.hasOwnProperty(key)) {
-            const business = businessData[key];
-            businesses.push(business);
-        }
+  for (const key in businessData) {
+    if (businessData.hasOwnProperty(key)) {
+      const business = businessData[key];
+      businesses.push(business);
     }
+  }
 
-    return businesses;
-}
+  return businesses;
+};
--- srv/utils/distanceCalculation.ts
+++ srv/utils/distanceCalculation.ts
@@ -1,43 +1,38 @@
 import { Business, SortedBusiness, UserLocation } from "../types/discoveryTypes";
 
 //helper
-export function sortByProximityToLocation(
-    businesses: Business[],
-    userLocation: UserLocation
-): SortedBusiness[] {
-    const sortedBusinesses: SortedBusiness[] = [];
+export function sortByProximityToLocation(businesses: Business[], userLocation: UserLocation): SortedBusiness[] {
+  const sortedBusinesses: SortedBusiness[] = [];
 
-    const userLat: number = userLocation.Lat
-    const userLong: number = userLocation.Long
-    // Calculate the distance between two points using the Haversine formula
-    function calculateDistance(lat1: number, long1: number, lat2: number, long2: number): number {
-        const earthRadius = 6371; // Earth's radius in kilometers
-        const dLat = (lat2 - lat1) * (Math.PI / 180);
-        const dLong = (long2 - long1) * (Math.PI / 180);
-        const a =
-            Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-            Math.cos((lat1 * Math.PI) / 180) *
-            Math.cos((lat2 * Math.PI) / 180) *
-            Math.sin(dLong / 2) *
-            Math.sin(dLong / 2);
-        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        const distance = earthRadius * c;
+  const userLat: number = userLocation.Lat;
+  const userLong: number = userLocation.Long;
+  // Calculate the distance between two points using the Haversine formula
+  function calculateDistance(lat1: number, long1: number, lat2: number, long2: number): number {
+    const earthRadius = 6371; // Earth's radius in kilometers
+    const dLat = (lat2 - lat1) * (Math.PI / 180);
+    const dLong = (long2 - long1) * (Math.PI / 180);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos((lat1 * Math.PI) / 180) * Math.cos((lat2 * Math.PI) / 180) * Math.sin(dLong / 2) * Math.sin(dLong / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = earthRadius * c;
 
-        return distance;
-    }
+    return distance;
+  }
 
-    businesses.forEach((business) => {
-        Object.entries(business.Locations).forEach(([locationId, location]) => {
-            const distance = calculateDistance(userLat, userLong, location.Lat, location.Long);
-            sortedBusinesses.push({
-                name: business["Basic Fields"]["Chain Name"],
-                locationId,
-                distance,
-            });
-        });
+  businesses.forEach((business) => {
+    Object.entries(business.Locations).forEach(([locationId, location]) => {
+      const distance = calculateDistance(userLat, userLong, location.Lat, location.Long);
+      sortedBusinesses.push({
+        name: business["Basic Fields"]["Chain Name"],
+        latitude: location.Lat,
+        longitude: location.Long,
+        distance,
+      });
     });
+  });
 
-    sortedBusinesses.sort((a, b) => a.distance - b.distance);
+  sortedBusinesses.sort((a, b) => a.distance - b.distance);
 
-    return sortedBusinesses;
+  return sortedBusinesses;
 }
